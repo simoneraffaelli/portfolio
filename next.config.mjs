32daa4acc3f1b3ebb229c11be0bc9de04e3f80c9
simoneraffaelli <ciao@simone.ooo>
2026-03-30 00:02:15 +0200

    enhance version command

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
+import { execSync } from 'child_process'
+
+const commitSha = execSync('git rev-parse --short HEAD').toString().trim()
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
+  },
+  env: {
+    NEXT_PUBLIC_COMMIT_SHA: commitSha,
   },
 }
 
